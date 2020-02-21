96b67d6b21a4d264e252c079f7c3a6b6782584ed
MatheoAtche <matheo.atche@insa-lyon.fr>
2020-02-21 16:25:21 +0100

    update main to avoid the need to recompile at each test

--- main.cpp
+++ main.cpp
@@ -4,15 +4,17 @@
 
 int main(void) {
    //pas besoin de new car pas d'allocation dynamique
-   string chaine("(1+34)*123");
+   cout << "Entrez votre formule (seulement à base d'entiers, multiplication, addition et parenthèses" << endl;
+   string chaine;
+   cin >> chaine;
    Lexer l(chaine);
    Automate a(&l);
    //deroulement de l'algo de bottom up
    a.lecture();
    cout<<"sorti de la lecture de l'automate"<<endl;   // debug
    Expr * resultat  =  a.popSymbol();
-   cout<<resultat->eval()<<endl;
-   cout<<"coucou c'est fini"<<endl;
+   cout << "Le résultat est :" << endl;
+   cout << resultat->eval() << endl;
    return 0;
 }
 
